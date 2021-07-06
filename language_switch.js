4c3dbb01d062632cb1965ee273f0b14d19aaed48
kawabe-lux <klux@posteo.de>
2021-07-06 06:16:38 +0200

    small changes to indentation and script structure

--- language_switch.js
+++ language_switch.js
@@ -1,17 +1,16 @@
 (function(){  
-	let schemeStored, languageStored
-	// is called before DOM is loaded
+  let languageStored
+  // is called before DOM is loaded
   function init() {
-		const root = document.documentElement;
+    const root = document.documentElement;
 
-		languageStored = sessionStorage.getItem('language');
-		if (languageStored == "DE"){
-			root.classList.add(languageStored);
-			root.setAttribute("lang", "de");
-		}
-		else if (languageStored == "EN"){
-			root.classList.add(languageStored);
-		}
+    languageStored = sessionStorage.getItem('language');
+    if (languageStored !== null){
+      root.classList.add(languageStored);
+      if (languageStored == "DE"){
+        root.setAttribute("lang", "de");
+      }
+    }
     
     // if DOM is already loaded, call onDOMLoad(). if not, listen to event
     if (document.readyState !== 'loading'){      
@@ -19,39 +18,39 @@
     } else{
       document.addEventListener('DOMContentLoaded', onDOMLoad);
     }
-	}
+  }
   
   // is called after DOM is loaded
   function onDOMLoad() {    
     setSwitch("language", function(pressed) {
-			let language;
-			if (pressed == true){
-				language = "DE";
-			}
-			else{
-				language = "EN";
-			}
-			document.documentElement.classList.toggle("DE");
-			sessionStorage.setItem('language', language);
-		})
+      let language;
+      if (pressed == false){
+        language = "DE";
+      }
+      else{
+        language = "EN";
+      }
+      document.documentElement.classList.toggle("DE");
+      sessionStorage.setItem('language', language);
+    })
     
     //sets switch to "DE" if stored so
     if (typeof languageStored != "undefined" && languageStored != null){
-			if (languageStored == "DE"){
-				document.getElementById("language-button").setAttribute("aria-checked", "false");
-			}
-		}
+      if (languageStored == "DE"){
+        document.getElementById("language-button").setAttribute("aria-checked", "false");
+      }
+    }
   }
   
   // function for dual switch function creation
   function setSwitch(name, callback) {
-		const button = document.getElementById(name + "-button");
-		button.addEventListener("click", function(){
-			const pressed = this.getAttribute("aria-checked") === "true";
+    const button = document.getElementById(name + "-button");
+    button.addEventListener("click", function(){
+      const pressed = this.getAttribute("aria-checked") === "true";
       this.setAttribute("aria-checked", !pressed);
-			callback(pressed);
-		});
-	}
+      callback(!pressed);
+    });
+  }
   
   init();
 })()
