95b52bbbf861d60026aa033896b6e46283dd0d39
Kawabe-Lux <kawabe-lux@posteo.de>
2021-10-19 13:01:32 +0200

    new HTML structure for better accessibility

--- language_switch.js
+++ language_switch.js
@@ -1,15 +1,13 @@
 (function(){  
   let languageStored
+  const root = document.documentElement;
+  const LANG_CODES = ["en", "de"];
+  const LANG_INDEXES = {"en": 0, "de": 1};
   // is called before DOM is loaded
   function init() {
-    const root = document.documentElement;
-
     languageStored = sessionStorage.getItem('language');
     if (languageStored !== null){
-      root.classList.add(languageStored);
-      if (languageStored == "DE"){
-        root.setAttribute("lang", "de");
-      }
+      root.setAttribute("lang", languageStored);
     }
     
     // if DOM is already loaded, call onDOMLoad(). if not, listen to event
@@ -22,34 +20,60 @@
   
   // is called after DOM is loaded
   function onDOMLoad() {    
-    setSwitch("language", function(pressed) {
-      let language;
-      if (pressed == false){
-        language = "DE";
-      }
-      else{
-        language = "EN";
-      }
-      document.documentElement.classList.toggle("DE");
+    setSwitch("language", function(language) {
+      root.setAttribute("lang", language);
       sessionStorage.setItem('language', language);
     })
     
     //sets switch to "DE" if stored so
     if (typeof languageStored != "undefined" && languageStored != null){
-      if (languageStored == "DE"){
-        document.getElementById("language-button").setAttribute("aria-checked", "false");
+      const radios = document.getElementById("language-switch").radios;
+      if (languageStored in radios){
+        radios[languageStored].checked = true;
+      }
+      else {
+        console.error("language-switch: Language stored not found in values"
+          + " stored in radio input values: " + languageStored);
       }
     }
   }
   
   // function for dual switch function creation
   function setSwitch(name, callback) {
-    const button = document.getElementById(name + "-button");
-    button.addEventListener("click", function(){
-      const pressed = this.getAttribute("aria-checked") === "true";
-      this.setAttribute("aria-checked", !pressed);
-      callback(!pressed);
-    });
+    const switchEl = document.getElementById(name + "-switch");
+    const radios = switchEl.getElementsByTagName("input");
+    /*switchEl.radios = radios;*/
+    switchEl.radios = {};
+    for (var i = radios.length - 1; i >= 0; i--) {
+      // set radio relationship
+      if (radios[i].checked == true) {
+        switchEl.radioChecked = radios[i];
+      }
+      radios[i].switch = switchEl;
+      if (i == radios.length - 1) {
+        radios[i].nextRadio = radios[0];
+      }
+      else {
+        radios[i].nextRadio = radios[i + 1];
+      }
+      switchEl.radios[radios[i].value] = radios[i];
+      // Click event
+      radios[i].addEventListener("click", function(event){
+        // check next if already checked
+        const checked = this.checked;
+        if (this === this.switch.radioChecked) {
+          // check next
+          this.switch.radioChecked = this.nextRadio;
+          this.checked = false;
+          this.nextRadio.checked = true;
+        }
+        else {
+          this.switch.radioChecked = this;
+        }
+        const value = this.switch.radioChecked.value;
+        callback(value);
+      });
+    }
   }
   
   init();
